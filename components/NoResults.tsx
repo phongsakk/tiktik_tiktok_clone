f1fb8f8dfe4d3999a3bf4daa1c2773ffd9859297
Phongsakk <phongsak.ysd@gmail.com>
2022-09-02 19:54:31 +0700

    no result components

--- components/NoResults.tsx
+++ components/NoResults.tsx
@@ -1,13 +1,40 @@
 import { NextPage } from 'next'
 import React from 'react'
+import { BiCommentX } from 'react-icons/bi'
+import { MdOutlineVideocamOff } from 'react-icons/md'
 
 interface IProps {
-  text: string
+  type: 'video' | 'comment'
 }
 
-const NoResults: NextPage<IProps> = ({ text }) => {
+const items = [
+  {
+    type: 'video',
+    icon: <MdOutlineVideocamOff />,
+    label: ''
+  },
+  {
+    type: 'comment',
+    icon: <BiCommentX />,
+    label: 'No comments yet! Be the first one to add a comment.'
+  }
+]
+
+const NoResults: NextPage<IProps> = ({ type }) => {
   return (
-    <div>NoResults</div>
+    <div className='flex flex-col justify-center items-center h-full w-full'>
+      {items.filter(item => item.type === type).map(item => (
+        <React.Fragment key={item.type}>
+          <p className='text-8xl'>
+            {item.icon}
+          </p>
+          <p className='text-2xl text-center'>
+            {item.label}
+          </p>
+        </React.Fragment>
+      ))}
+
+    </div>
   )
 }
 
