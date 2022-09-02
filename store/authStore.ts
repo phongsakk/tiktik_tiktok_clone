0ab6f79f1102db05f65334afabc259902721404d
Phongsakk <phongsak.ysd@gmail.com>
2022-09-02 22:19:25 +0700

    suggested user

--- store/authStore.ts
+++ store/authStore.ts
@@ -1,12 +1,19 @@
 import create from 'zustand'
 import { persist } from 'zustand/middleware'
 import axios from 'axios'
+
 import { IUser } from '../types'
+import { BASE_URL } from '../utils'
 
 const authStore = (set: any): AuthStore => ({
-  userProfile: null,
+  userProfile: undefined,
+  allUsers: [],
   addUser: (user: IUser) => set({ userProfile: user }),
   removeUser: () => set({ userProfile: null }),
+  fetchAllUsers: async () => {
+    const response = await axios.get(`${BASE_URL}/api/users`)
+    set({ allUsers: response.data })
+  }
 })
 
 const useAuthStore = create(
@@ -16,9 +23,11 @@ const useAuthStore = create(
 )
 
 export interface AuthStore {
-  userProfile: IUser | null
+  userProfile?: IUser
+  allUsers: IUser[]
   addUser: (user: IUser) => void
   removeUser: () => void
+  fetchAllUsers: () => unknown
 }
 
 export default useAuthStore
