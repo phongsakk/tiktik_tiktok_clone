d2d74a0a6d0a37c7d9521e67d25157209d392c36
Phongsakk <phongsak.ysd@gmail.com>
2022-09-02 14:30:51 +0700

    remove comment

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,4 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { PostPayload } from '../../../types'
 import { client } from '../../../utils/client'
